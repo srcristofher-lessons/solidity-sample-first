7e2672267e8bede17ed25bf59cc7bd503a423bae
aybike <tonn.raus09@gmail.com>
2021-09-02 16:38:09 +0200

    feat: delete package lock json

--- contracts/hello.sol
+++ contracts/hello.sol
@@ -1,12 +1,22 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract Hello {
-	string public message;
-	function hello(string memory initialMessage) public {
-		message = initialMessage;
+contract FirstContract {
+	uint count;
+
+	constructor() {
+		count = 10;
 	}
-	function setMessage(string memory newMessage) public {
-		message = newMessage;
+
+	function getCount() public view returns(uint) {
+		return count;
+	}
+
+	function addCount(uint _number) public {
+		count = count + _number;
+	}
+
+	function substractCount(uint _number) public {
+		count = count - _number;
 	}
 }
